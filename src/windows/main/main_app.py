dcdaa4a8295baba62b9f9c7f13ac281350f1d709
Mineeagle <streber123@streber24.de>
2026-06-02 20:56:32 +0200

    Reset window size

--- src/windows/main/main_app.py
+++ src/windows/main/main_app.py
@@ -47,7 +47,7 @@ class MainApp(Window):
     """Main windows of the application"""
 
     def __init__(self):
-        super().__init__("PyMacroRecord", 700, 400)
+        super().__init__("PyMacroRecord", 350, 200)
         self.attributes("-topmost", 1)
         if platform == "win32":
             self.iconbitmap(resource_path(path.join("assets", "logo.ico")))
